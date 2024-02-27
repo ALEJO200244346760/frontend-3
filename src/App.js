00396a45516fa6cae4bdd7fe7dbd5db644de755e
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-02-27 02:56:22 -0300

    Agregar archivos iniciales de template-react

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
